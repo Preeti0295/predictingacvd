60df3f17de68c7f997b562a489fd1ec54cc9001f
Preeti0295 <63549304+Preeti0295@users.noreply.github.com>
2020-04-17 01:23:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from sklearn.linear_model import LinearRegression
+
+import pickle
+
+app = Flask(__name__)
+# app.run(host='0.0.0.0', port=8080,debug=True)
+lr = pickle.load(open('lr.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = lr.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Prediction is {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = lr.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
+   
